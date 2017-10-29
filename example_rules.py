de226dfc5ec068e8b976232f5c158f488b13a58d
jonasc <github@bcdf.eu>
2017-10-29 17:12:47 +0100

    introduce example rules

---
+++ example_rules.py
@@ -0,0 +1,12 @@
+def get_rules():
+    return {
+        # Move all mails matching the following rules to folder "Lists"
+        'Lists': [
+            # All emails to job mailing list
+            ('HEADER', 'List-Id', 'jobs@example.org'),
+        ],
+        'Work': [
+            ('FROM', 'bob@example.org'),
+            ('FROM', 'alice@example.com'),
+        ]
+    }
